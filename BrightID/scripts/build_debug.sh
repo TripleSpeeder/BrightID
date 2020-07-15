3acb3f9ba04018b3a04f87e519479b1ce6db40e2
Michael Bauer <michael@m-bauer.org>
2020-07-15 22:45:45 +0200

    more tests

--- BrightID/scripts/build_debug.sh
+++ BrightID/scripts/build_debug.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 touch ./android/app/src/main/assets/index.android.bundle
+echo "bundle"
 ./node_modules/.bin/react-native bundle --dev false --platform android --entry-file index.js --bundle-output ./android/app/src/main/assets/index.android.bundle --assets-dest ./android/app/src/main/res/
+echo "assembleDebug"
 cd android && ./gradlew assembleDebug && cd ..
+
